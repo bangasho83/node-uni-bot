44a1eb2c3cc690dd45eb34517270686ee0289ae5
bangasho83 <omair.bangash@tossdown.com>
2025-02-15 12:23:54 -0500

    8

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+console.log("HELLO BHAI JAAN");
 
 const app = express();
 const port = process.env.PORT || 3000;
